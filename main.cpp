6225779772625c48f22d08671f952431c007e592
Maxim Pochinskiy <mvp280102@gmail.com>
2022-11-23 14:09:26 +0500

    Add reading input data from file.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,28 @@
-int main()
+#include "px_func.h"
+#include "qx_func.h"
+#include "fxy_func.h"
+
+#include <cstdio>
+#include <cstdlib>
+
+typedef unsigned int uint;
+
+int main(int argc, char *argv[])
 {
+	double x0, x1, y0, y1, h, eps;
+
+	FILE *in_file = fopen(argv[1], "r"),
+			*out_file = fopen(argv[2], "w");
+
+	if (in_file != nullptr)
+	{
+		fscanf_s(in_file, "%lf %lf %lf %lf %lf %lf", &x0, &x1, &y0, &y1, &h, &eps);
+	}
+	else
+	{
+		printf("Input file opening error!\nAborting program!\n");
+		exit(1);
+	}
+
 	return 0;
 }
