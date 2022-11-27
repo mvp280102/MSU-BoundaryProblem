0aa96862fdd9c12cd909101a518481caf9395b70
Maxim Pochinskiy <mvp280102@gmail.com>
2022-11-27 09:32:26 +0500

    Common structure without Runge accuracy evaluation method and plot drawing.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,59 @@
-#include "px_func.h"
-#include "qx_func.h"
-#include "fxy_func.h"
+#include "solver.h"
 
 #include <cstdio>
 #include <cstdlib>
 
-typedef unsigned int uint;
+void show_array(uint len, double *arr)
+{
+	for (uint i = 0; i < len; ++i)
+		printf("%f ", arr[i]);
+
+	printf("\n");
+}
 
 int main(int argc, char *argv[])
 {
-	double x0, x1, y0, y1, h, eps;
+	double eps, der_a, diff, *y_array;
+
+	BoundaryData data = {};
 
 	FILE *in_file = fopen(argv[1], "r"),
-			*out_file = fopen(argv[2], "w");
+		 *out_file = fopen(argv[2], "w");
 
-	if (in_file != nullptr)
+	if (in_file && out_file)
 	{
-		fscanf_s(in_file, "%lf %lf %lf %lf %lf %lf", &x0, &x1, &y0, &y1, &h, &eps);
+		fscanf_s(in_file, "%lf %lf %lf %lf %lf %d", &data.arg_a, &data.arg_b, &data.func_a, &data.func_b, &eps, &data.intervals);
+
+		y_array = (double*)malloc(sizeof(double) * (data.intervals + 1));
+
+		diff = boundary_solve(data, -1, y_array);
+
+		while (fabs(diff) > eps)
+		{
+			der_a = newton_step(data, diff, y_array, boundary_solve);
+			diff = boundary_solve(data, der_a, y_array);
+		}
+
+		printf("der_a = %lf\n\n", der_a);
+
+		printf("y_array:\n");
+		show_array(data.intervals + 1, y_array);
+
+		fprintf(out_file, "%lf %lf %d\n", data.arg_a, data.arg_b, data.intervals);
+
+		for (uint i = 0; i < data.intervals + 1; ++i)
+			fprintf(out_file, "%lf\n", y_array[i]);
+
+		system("python plot.py");
+
+		free(y_array);
+
+		fclose(out_file);
+		fclose(in_file);
 	}
 	else
 	{
-		printf("Input file opening error!\nAborting program!\n");
+		printf("File opening error!\nAborting program!\n");
 		exit(1);
 	}
 
