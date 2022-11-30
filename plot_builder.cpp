2a12df6981a61eb551a31b043627d7418b542ccf
Maxim Pochinskiy <mvp280102@gmail.com>
2022-11-30 15:53:07 +0500

    Add plot building (to PNG-image).

---
+++ plot_builder.cpp
@@ -0,0 +1,21 @@
+#include "plot_builder.h"
+
+void plot_build(uint size, uint len, double *arg, double *res, char* filename)
+{
+	RGBABitmapImageReference *canvas_reference = CreateRGBABitmapImageReference();
+	StringReference *error_message = CreateStringReference((wchar_t*)"Plot building error!\n", 21);
+
+	if (DrawScatterPlot(canvas_reference, size, size, arg, len, res, len, error_message))
+	{
+		size_t length;
+		double *png_data = ConvertToPNG(&length, canvas_reference->image);
+
+		WriteToFile(png_data, length, filename);
+		DeleteImage(canvas_reference->image);
+	}
+	else
+	{
+		printf("%ls\nAborting program!\n", error_message->string);
+		exit(1);
+	}
+}
---
+++ plot_builder.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "headers.h"
+
+void plot_build(uint size, uint len, double *arg, double *res, char* filename);
