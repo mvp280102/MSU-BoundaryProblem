4186151483156d64a1439012e64e967c09f5ad28
Maxim Pochinskiy <mvp280102@gmail.com>
2022-12-17 18:34:54 +0500

    Add comments for p(x), q(x) and f(x, y) functions.

---
+++ func.c
@@ -0,0 +1,26 @@
+#include "func.h"
+
+
+// Возвращает значение точного решения краевой задачи.
+double yx_func(double x)
+{
+    return 0;
+}
+
+// Возвращает значение коэффициента p(x).
+double px_func(double x)
+{
+	return -1 * (x + 4) / (2 * x);
+}
+
+// Возвращает значение коэффициента q(x).
+double qx_func(double x)
+{
+	return (1 + 4 / x) / (2 * x);
+}
+
+// Возвращает значение правой части f(x, y).
+double fxy_func(double x, double y)
+{
+	return pow(x, 3) / (2 * x);
+}
